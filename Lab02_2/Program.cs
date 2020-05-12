c01adec5d7c6e951215484166ce298959b00e643
Yura <rybakyra@gmail.com>
2020-05-12 14:50:04 +0300

    lab02_2

---
+++ Lab02_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab02_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("|  X:  | Y=f(x): |");
+            double a = 0, b = 3, dx = 0.05 , y ,x =a;
+            while (x <= b)
+            {
+                y = Math.Pow(4, -Math.Cos(x));
+                y = Math.Round(y, 4);
+                Console.WriteLine("| {0,-4} | {1,-7} |", x, y);
+                x += dx;
+                x = Math.Round(x, 2);
+            }
+        }
+    }
+}
